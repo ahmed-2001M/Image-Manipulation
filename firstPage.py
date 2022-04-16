0b0b6fb1f70ac56c0ae5f5318b907b71982a8b47
ahmed <ahme.abdallah124@gmail.com>
2022-04-16 04:56:32 +0200

    i am maked file readable

--- firstPage.py
+++ firstPage.py
@@ -4,11 +4,16 @@ import os
 import pathlib
 
 # Colores
-LIGHT_GRAY_BUTTON_COLOR = f'#212021 on #e0e0e0'
-DARK_GRAY_BUTTON_COLOR = '#e0e0e0 on #212021'
+FIRST_COLOR = f'#E7E7E7 on #041B2D'
+SECOND_COLOR = '#041B2D on #9B9B9B '
+sg.set_options(element_padding=(1, 2))
 
+# Menu bar
+menu_def = [
+    ['&File', ['&Open (Ctrl+O)', '&Save', '&Properties', 'E&xit']],
+]
 
-
+# Open function (Menu)
 def open_file():
     '''Open file and update the infobar'''
     filename = sg.popup_get_file('Open', no_window=True)
@@ -17,62 +22,65 @@ def open_file():
         return file
 
 
+# Buttons for manibulate image (every button has many buttons)
+left_col = [
+    [sg.B('GrayScale', size=(20, 1), button_color=SECOND_COLOR)],
+    [sg.B('difference', size=(20, 1), button_color=SECOND_COLOR)],
+    [sg.B('Contrast Stretching', size=(20, 1), button_color=SECOND_COLOR)],
+    [sg.B('filter', size=(20, 1), button_color=SECOND_COLOR)]
 
-
-sg.set_options(element_padding=(1, 2))
-
-menu_def = [
-    ['&File', ['&Open (Ctrl+O)', '&Save', '&Properties', 'E&xit' ]],
-        ]
-
-
-
-
-filter_bar = [
-    [sg.B('median filter',size = (20,1),button_color =LIGHT_GRAY_BUTTON_COLOR),sg.B('avg filter',size = (20,1), button_color =LIGHT_GRAY_BUTTON_COLOR)]
 ]
+
+# filter_buttons
+filter_buttons = [
+    [sg.B('median filter', size=(20, 1), button_color=FIRST_COLOR), sg.B('avg filter', size=(20, 1), button_color=FIRST_COLOR)]
+]
+
+# Will Change######################################
 one = [
-    [sg.B('wow filter',size = (20,1)),sg.B('gogo filter',size = (20,1)),sg.B('MONMO filter',size = (20,1))]
+    [sg.B('wow filter', size=(20, 1)), sg.B('gogo filter', size=(20, 1)), sg.B('MONMO filter', size=(20, 1))]
 ]
-left_bar = [
-    [sg.B('GrayScale',size = (20,1), button_color=DARK_GRAY_BUTTON_COLOR )],
-    [sg.B('difference',size = (20,1), button_color=DARK_GRAY_BUTTON_COLOR )],
-    [sg.B('Contrast Stretching',size = (20,1), button_color=DARK_GRAY_BUTTON_COLOR )],
-    [sg.B('filter',size = (20,1), button_color=DARK_GRAY_BUTTON_COLOR )]
-    
-]
-mid_bar =[
-    
+
+# Will Show Image and histo grame
+mid_col = [
+
     [sg.Text("import image:")],
     [sg.Text(size=(40, 1), key="-TOUT-")],
     [sg.Image(key="-IMAGE-")],
 ]
 
+#Make Histo Grame######################################
+
+#######################################################
 
 
-
+# layout to put elements on window
 layout = [
     [sg.MenubarCustom(menu_def, tearoff=False)],
-    [sg.Column(left_bar, key='-left-'),sg.Column(mid_bar,key='-mid-')],
-    [sg.Column(filter_bar,key='-filter-'),sg.Column(one, visible=False, key='-GrayScale-')]
-    ]
+    [sg.Column(left_col, key='-left-'), sg.Column(mid_col, key='-mid-')],
+    [sg.Column(filter_buttons, key='-filter-'), sg.Column(one, visible=False, key='-GrayScale-')]
+]
 
-
-window = sg.Window('Window Title', layout ,margins = (0,0), resizable=True, return_keyboard_events=True, finalize=True)
+# Display Window
+window = sg.Window('Window Title', layout, margins=(
+    0, 0), resizable=True, return_keyboard_events=True, finalize=True)
 window.maximize()
 
-last='filter'
+
+active = 'filter'  # To Save Active Button
+
+# running loop
 while True:
     event, values = window.read()
     print(event, values)
     if event in (None, 'Exit'):
         break
-    
-    if event in ['filter','GrayScale'] :
-        window[f'-{last}-'].update(visible=False)
-        last = event
-        print(last)
-        window[f'-{last}-'].update(visible=True)
+
+    if event in ['filter', 'GrayScale']:
+        window[f'-{active}-'].update(visible=False)
+        active = event
+        print(active)
+        window[f'-{active}-'].update(visible=True)
     if event in ('Open (Ctrl+O)', 'o:79'):
         file = open_file()
         print(file)
